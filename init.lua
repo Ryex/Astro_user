fb8649fe4f99b0da8c90258c8dbbd89736220110
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-03-21 08:47:00 -0700

    personal config

--- init.lua
+++ init.lua
@@ -16,16 +16,10 @@ return {
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     },
   },
-
   -- Set colorscheme to use
   colorscheme = "astrodark",
-
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-  diagnostics = {
-    virtual_text = true,
-    underline = true,
-  },
-
+  diagnostics = { virtual_text = true, underline = true },
   lsp = {
     -- customize lsp formatting options
     formatting = {
@@ -51,19 +45,25 @@ return {
     servers = {
       -- "pyright"
     },
+    config = { clangd = { capabilities = { offsetEncoding = "utf-16" } } },
   },
-
   -- Configure require("lazy").setup() options
   lazy = {
     defaults = { lazy = true },
     performance = {
       rtp = {
         -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
+        disabled_plugins = {
+          "tohtml",
+          "gzip",
+          "matchit",
+          "zipPlugin",
+          "netrwPlugin",
+          "tarPlugin",
+        },
       },
     },
   },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
@@ -80,5 +80,44 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.o.guifont = "FiraCode Nerd Font Mono:h8" -- text below applies for VimScript
+    vim.opt.linespace = 0
+    vim.g.neovide_scale_factor = 1.0
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
+
+    -- Helper function for transparency formatting
+    local alpha = function() return string.format("%x", math.floor(255 * vim.g.transparency or 0.8)) end
+    -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+    vim.g.neovide_transparency = 1.0
+    vim.g.transparency = 1.0
+    -- vim.g.neovide_background_color = "#0f1117" .. alpha()
+
+    vim.g.neovide_floating_blur_amount_x = 2.0
+    vim.g.neovide_floating_blur_amount_y = 2.0
+
+    vim.g.neovide_scroll_animation_length = 0.3
+
+    vim.g.neovide_hide_mouse_when_typing = false
+
+    vim.g.neovide_underline_automatic_scaling = false
+    vim.g.neovide_refresh_rate = 30
+    vim.g.neovide_refresh_rate_idle = 5
+    vim.g.neovide_cursor_animation_length = 0.13
+    vim.g.neovide_cursor_trail_size = 0.8
+    vim.g.neovide_cursor_antialiasing = true
+    vim.g.neovide_cursor_animate_in_insert_mode = true
+    vim.g.neovide_cursor_animate_command_line = true
+    vim.g.neovide_cursor_unfocused_outline_width = 0.125
+
+    vim.g.neovide_cursor_vfx_mode = "railgun"
+    vim.g.neovide_cursor_vfx_opacity = 200.0
+    vim.g.neovide_cursor_vfx_particle_density = 7.0
+    vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
+    vim.g.neovide_cursor_vfx_particle_speed = 10.0
+    vim.g.neovide_cursor_vfx_particle_phase = 1.5
+    vim.g.neovide_cursor_vfx_particle_curl = 1.0
   end,
 }
