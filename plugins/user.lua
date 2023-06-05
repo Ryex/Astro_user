c2b30500e3d8dd8970d9b07be8add91af34c8e17
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-06-04 23:57:39 -0700

    add search and replace; fix inlayhints toggle
    Signed-off-by: Rachel Powers <508861+Ryex@users.noreply.github.com>

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,8 +11,25 @@ return {
   -- },
   {
     "Djancyp/custom-theme.nvim",
+    config = function() require("custom-theme").setup() end,
+  },
+  {
+    "s1n7ax/nvim-search-and-replace",
     config = function()
-        require("custom-theme").setup()
+      require("nvim-search-and-replace").setup {
+        -- file patters to ignore
+        ignore = {'**/node_modules/**', '**/.git/**',  '**/.gitignore', '**/.gitmodules','build/**'},
+        -- save the changes after replace
+        update_changes = false,
+        -- keymap for search and replace
+        replace_keymap = "<leader>gr",
+        -- keymap for search and replace ( this does not care about ignored files )
+        replace_all_keymap = "<leader>gR",
+        -- keymap for search and replace
+        replace_and_save_keymap = "<leader>gu",
+        -- keymap for search and replace ( this does not care about ignored files )
+        replace_all_and_save_keymap = "<leader>gU",
+      }
     end,
   },
 }
