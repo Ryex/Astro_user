a80c0bbce6041dc49f7b025d890807d14dfe9e67
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-07-01 03:31:14 -0700

    more neovide settings
    Signed-off-by: Rachel Powers <508861+Ryex@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -186,7 +186,8 @@ return {
       vim.g.neovide_floating_blur_amount_x = 2.0
       vim.g.neovide_floating_blur_amount_y = 2.0
 
-      vim.g.neovide_scroll_animation_length = 0.2
+      vim.g.neovide_scroll_animation_length = 0.3
+      vim.g.neovide_hide_mouse_when_typing = true
 
       vim.g.neovide_underline_automatic_scaling = false
 
@@ -206,6 +207,8 @@ return {
       vim.g.neovide_refresh_rate = 60
       vim.g.neovide_refresh_rate_idle = 5
       vim.g.neovide_no_idle = false
+
+      vim.g.neovide_confirm_quit = true
 
       vim.g.neovide_cursor_animation_length = 0.13
       -- vim.g.neovide_cursor_animation_length = 0 -- disable
@@ -219,7 +222,8 @@ return {
       vim.g.neovide_cursor_antialiasing = true
       vim.g.neovide_cursor_unfocused_outline_width = 0.125
 
-      vim.g.neovide_cursor_vfx_mode = "railgun"
+      -- vim.g.neovide_cursor_vfx_mode = "railgun"
+      vim.g.neovide_cursor_vfx_mode = "pixiedust"
       vim.g.neovide_cursor_vfx_opacity = 200.0
       vim.g.neovide_cursor_vfx_particle_density = 7.0
       vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
