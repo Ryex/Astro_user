a9988701a867f73e54e05992b820645bf2a6c129
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-07-01 03:03:22 -0700

    load vsdevcmd envars on windows (use vswhere to locate)
    Signed-off-by: Rachel Powers <508861+Ryex@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -1,9 +1,26 @@
 require "user.reload"
 
-if vim.fn.has('win32') then
+if vim.fn.has "win32" then
+  -- run, capture, setenv from vsdevcmd
+  local res = vim.fn.system { "vswhere", "-latest", "-property", "installationPath" }
+  if res ~= "" then
+    print "Setting Dev env vars from vsdevcmd.bat"
+    local vsdevcmd_path = res:gsub("^%s*(.-)%s*$", "%1") .. "\\Common7\\Tools\\vsdevcmd.bat"
+    local vsdev_res = vim.fn.system { vim.env.comspec, "/C", '"' .. vsdevcmd_path .. '"' .. " -no_logo && set" }
+    for line in vsdev_res:gmatch "[^\n\r]+" do
+      local s, e = string.find(line, "=", 0, true)
+      if s ~= nil then
+        local name = line:sub(0, s - 1)
+        local value = line:sub(e + 1, #line)
+        vim.fn.setenv(name, value)
+      end
+    end
+  end
+
   local powershell_options = {
     shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
     shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
     shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
     shellquote = "",
@@ -18,16 +35,16 @@ end
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly",    -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
+    auto_quit = false,     -- automatically quit the current session after a successful update
+    remotes = {            -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -54,7 +71,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = false, -- enable or disable format on save globally
+        enabled = false,    -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
           "json",
